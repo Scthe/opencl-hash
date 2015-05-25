3f963605294ac9d66d7cbcc9ff27c1ba9ccc9440
Scthe <marcin1113C@gmail.com>
2015-05-26 00:58:55 +0200

    Format files

--- src/Main_cl.cpp
+++ src/Main_cl.cpp
@@ -7,182 +7,204 @@
 #include <windows.h>
 
 // const char* cSourceFile = "./open_cl_kernel.cl";
-const char* cSourceFile = "src/kernel/open_cl_kernel.cl";
+const char *cSourceFile = "src/kernel/open_cl_kernel.cl";
 typedef unsigned long long ull;
 
 // OpenCL Vars
-cl_context cxGPUContext;        // OpenCL context
-cl_command_queue cqCommandQueue;// OpenCL command queue
-cl_device_id cdDevice;          // OpenCL device
-cl_program cpProgram;           // OpenCL program
-cl_kernel ckKernel;             // OpenCL kernel
-char* cSourceCL = nullptr;      // Buffer to hold source for compilation
+cl_context cxGPUContext;         // OpenCL context
+cl_command_queue cqCommandQueue; // OpenCL command queue
+cl_device_id cdDevice;           // OpenCL device
+cl_program cpProgram;            // OpenCL program
+cl_kernel ckKernel;              // OpenCL kernel
+char *cSourceCL = nullptr;       // Buffer to hold source for compilation
 
 // forward declaration
 void Cleanup(int argc, char **argv, int iExitCode);
-void checkError(int argc, char **argv, cl_int ciErr, char const * msg);
-
+void checkError(int argc, char **argv, cl_int ciErr, char const *msg);
 
 // Problem variables
-void *dst;        // Host buffers for OpenCL test
-cl_mem cmDevDst;  // OpenCL device destination buffer
-
+void *dst;       // Host buffers for OpenCL test
+cl_mem cmDevDst; // OpenCL device destination buffer
 
 // main
-int main(int argc, char **argv){
-	std::cout << "start" << '\n';
-	ull iter_count = ((ull) 1) << (4*9); // 16^9
+int main(int argc, char **argv) {
+  std::cout << "start" << '\n';
+  ull iter_count = ((ull)1) << (4 * 9); // 16^9
 
-	// set and log Global and Local work size dimensions
-	size_t szLocalWorkSize = 256;  // 1D var for # of work items in the work group
-	// 1D var for Total # of work items
-	//( rounded up to the nearest multiple of the LocalWorkSize)
-	size_t szGlobalWorkSize = 256 * 256;// 16^4
+  // set and log Global and Local work size dimensions
+  size_t szLocalWorkSize = 256; // 1D var for # of work items in the work group
+  // 1D var for Total # of work items
+  //( rounded up to the nearest multiple of the LocalWorkSize)
+  size_t szGlobalWorkSize = 256 * 256; // 16^4
 
-	cl_int ciErr1;
+  cl_int ciErr1;
 
-	// Allocate and initialize host< c++> arrays
-	dst = (void *)malloc(sizeof(cl_char)* 1024);
+  // Allocate and initialize host< c++> arrays
+  dst = (void *)malloc(sizeof(cl_char) * 1024);
 
-	//Get an OpenCL platform
-	cl_platform_id cpPlatform;
-	ciErr1 = clGetPlatformIDs(1, &cpPlatform, nullptr);
-	checkError(argc, argv, ciErr1, "Error in clGetPlatformID");
+  // Get an OpenCL platform
+  cl_platform_id cpPlatform;
+  ciErr1 = clGetPlatformIDs(1, &cpPlatform, nullptr);
+  checkError(argc, argv, ciErr1, "Error in clGetPlatformID");
 
-	//Get the devices
-	ciErr1 = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &cdDevice, nullptr);
-	checkError(argc, argv, ciErr1, "Error in clGetDeviceIDs");
+  // Get the devices
+  ciErr1 =
+      clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &cdDevice, nullptr);
+  checkError(argc, argv, ciErr1, "Error in clGetDeviceIDs");
 
-	//Create the context
-	cxGPUContext = clCreateContext(0, 1, &cdDevice, nullptr, nullptr, &ciErr1);
-	checkError(argc, argv, ciErr1, "Error in clCreateContext");
+  // Create the context
+  cxGPUContext = clCreateContext(0, 1, &cdDevice, nullptr, nullptr, &ciErr1);
+  checkError(argc, argv, ciErr1, "Error in clCreateContext");
 
-	// Create a command-queue
-	cqCommandQueue = clCreateCommandQueue(cxGPUContext, cdDevice, 0, &ciErr1);
-	checkError(argc, argv, ciErr1, "Error in clCreateCommandQueue");
+  // Create a command-queue
+  cqCommandQueue = clCreateCommandQueue(cxGPUContext, cdDevice, 0, &ciErr1);
+  checkError(argc, argv, ciErr1, "Error in clCreateCommandQueue");
 
-	// Allocate the OpenCL buffers
-	// for source and result on the device GMEM
-	cmDevDst = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE, sizeof(cl_char)* 1024, nullptr, &ciErr1);
-	checkError(argc, argv, ciErr1, "Error in clCreateBuffer");
+  // Allocate the OpenCL buffers
+  // for source and result on the device GMEM
+  cmDevDst = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE,
+                            sizeof(cl_char) * 1024, nullptr, &ciErr1);
+  checkError(argc, argv, ciErr1, "Error in clCreateBuffer");
 
-	// Read the OpenCL kernel in from source file
-	size_t szKernelLength = 0;
-	std::cout << "Reading kernel function from '" << cSourceFile << "'" << '\n';
-	cSourceCL = oclLoadProgSource(cSourceFile, "", &szKernelLength);
-	std::cout << "Kernel length: " << szKernelLength << std::endl;
-	checkError(argc, argv, szKernelLength > 0? CL_SUCCESS :CL_INVALID_PROGRAM, "Error in clCreateProgramWithSource");
+  // Read the OpenCL kernel in from source file
+  size_t szKernelLength = 0;
+  std::cout << "Reading kernel function from '" << cSourceFile << "'" << '\n';
+  cSourceCL = oclLoadProgSource(cSourceFile, "", &szKernelLength);
+  std::cout << "Kernel length: " << szKernelLength << std::endl;
+  checkError(argc, argv, szKernelLength > 0 ? CL_SUCCESS : CL_INVALID_PROGRAM,
+             "Error in clCreateProgramWithSource");
 
-	// Create & build the program
-	cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSourceCL, &szKernelLength, &ciErr1);
-	checkError(argc, argv, ciErr1, "Error in clCreateProgramWithSource");
-	// http://www.khronos.org/registry/cl/sdk/2.0/docs/man/xhtml/clBuildProgram.html
-	ciErr1 = clBuildProgram(cpProgram, 0, nullptr, nullptr, nullptr, nullptr);
-	if (ciErr1 == CL_BUILD_PROGRAM_FAILURE){
-		size_t length;
-		char buffer[2048];
-		clGetProgramBuildInfo(cpProgram, cdDevice, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &length);
-		std::cout << "--- Build log ---\n " << buffer << '\n';
-	}
-	checkError(argc, argv, ciErr1, "Error in clBuildProgram");
+  // Create & build the program
+  cpProgram = clCreateProgramWithSource(
+      cxGPUContext, 1, (const char **)&cSourceCL, &szKernelLength, &ciErr1);
+  checkError(argc, argv, ciErr1, "Error in clCreateProgramWithSource");
+  // http://www.khronos.org/registry/cl/sdk/2.0/docs/man/xhtml/clBuildProgram.html
+  ciErr1 = clBuildProgram(cpProgram, 0, nullptr, nullptr, nullptr, nullptr);
+  if (ciErr1 == CL_BUILD_PROGRAM_FAILURE) {
+    size_t length;
+    char buffer[2048];
+    clGetProgramBuildInfo(cpProgram, cdDevice, CL_PROGRAM_BUILD_LOG,
+                          sizeof(buffer), buffer, &length);
+    std::cout << "--- Build log ---\n " << buffer << '\n';
+  }
+  checkError(argc, argv, ciErr1, "Error in clBuildProgram");
 
-	// Create the kernel
-	//cl_kernel clCreateKernel(cl_program  program, const char *kernel_name, cl_int *errcode_ret)
-	ckKernel = clCreateKernel(cpProgram, "HashKernel", &ciErr1);
-	checkError(argc, argv, ciErr1, "Error in clCreateKernel");
+  // Create the kernel
+  // cl_kernel clCreateKernel(cl_program  program, const char *kernel_name,
+  // cl_int *errcode_ret)
+  ckKernel = clCreateKernel(cpProgram, "HashKernel", &ciErr1);
+  checkError(argc, argv, ciErr1, "Error in clCreateKernel");
 
-	auto repeatCnt = iter_count / szGlobalWorkSize;
-	//void* nullBuffer = malloc(sizeof(cl_char)* 1024);
-	char nullBuffer[sizeof(cl_char)* 1024];
-	memset(nullBuffer, 0, 1024);
-	int per = 0;
-	for (ull i = 0; i < repeatCnt; i++){
-		if (i % (repeatCnt / 100) == 0){
-			std::cout << per << " %" << '\n';
-			++per;
-		}
+  auto repeatCnt = iter_count / szGlobalWorkSize;
+  // void* nullBuffer = malloc(sizeof(cl_char)* 1024);
+  char nullBuffer[sizeof(cl_char) * 1024];
+  memset(nullBuffer, 0, 1024);
+  int per = 0;
+  for (ull i = 0; i < repeatCnt; i++) {
+    if (i % (repeatCnt / 100) == 0) {
+      std::cout << per << " %" << '\n';
+      ++per;
+    }
 
-		// Set the Argument values
-		//cl_int clSetKernelArg(cl_kernel kernel, cl_uint arg_index, size_t arg_size, const void *arg_value)
-		ciErr1 = clSetKernelArg(ckKernel, 0, sizeof(cl_mem), (void*)&cmDevDst);
-		ciErr1 |= clSetKernelArg(ckKernel, 1, sizeof(cl_int), (void*)&i);
-		checkError(argc, argv, ciErr1, "Error in clSetKernelArg");
+    // Set the Argument values
+    // cl_int clSetKernelArg(cl_kernel kernel, cl_uint arg_index, size_t
+    // arg_size, const void *arg_value)
+    ciErr1 = clSetKernelArg(ckKernel, 0, sizeof(cl_mem), (void *)&cmDevDst);
+    ciErr1 |= clSetKernelArg(ckKernel, 1, sizeof(cl_int), (void *)&i);
+    checkError(argc, argv, ciErr1, "Error in clSetKernelArg");
 
-		// Enqueues a command to execute a kernel on a device.
-		/*
-		// copy from host to device
-		ciErr1 = clEnqueueWriteBuffer(cqCommandQueue,	// cl_command_queue command_queue
-									  cmDevDst,	// cl_mem buffer
-									  CL_FALSE,		// cl_bool blocking_write
-									  0,			// size_t offset
-									  sizeof(cl_char)* 1024, // size_t cb
-									  memset,			// const void *ptr
-									  0,			// cl_uint num_events_in_wait_list
-									  nullptr,		// const cl_event *event_wait_list
-									  nullptr);		// cl_event *event
-		*/
+    // Enqueues a command to execute a kernel on a device.
+    /*
+    // copy from host to device
+    ciErr1 = clEnqueueWriteBuffer(cqCommandQueue,	// cl_command_queue
+    command_queue
+                                                              cmDevDst,	//
+    cl_mem buffer
+                                                              CL_FALSE,		//
+    cl_bool blocking_write
+                                                              0,			// size_t
+    offset
+                                                              sizeof(cl_char)*
+    1024, // size_t cb
+                                                              memset,			//
+    const void *ptr
+                                                              0,			// cl_uint
+    num_events_in_wait_list
+                                                              nullptr,		//
+    const cl_event *event_wait_list
+                                                              nullptr);		//
+    cl_event *event
+    */
 
-		// Launch kernel
-		ciErr1 = clEnqueueNDRangeKernel(cqCommandQueue,	// cl_command_queue command_queue
-										ckKernel,		// cl_kernel kernel
-										1,				// cl_uint work_dim
-										nullptr,		// const size_t *global_work_offset
-										&szGlobalWorkSize,	// const size_t *global_work_size
-										&szLocalWorkSize,	// const size_t *local_work_size
-										0,				// cl_uint num_events_in_wait_list
-										nullptr,		// const cl_event *event_wait_list
-										nullptr);		// cl_event *event
-		checkError(argc, argv, ciErr1, "Error in clEnqueueNDRangeKernel");
+    // Launch kernel
+    ciErr1 = clEnqueueNDRangeKernel(
+        cqCommandQueue,    // cl_command_queue command_queue
+        ckKernel,          // cl_kernel kernel
+        1,                 // cl_uint work_dim
+        nullptr,           // const size_t *global_work_offset
+        &szGlobalWorkSize, // const size_t *global_work_size
+        &szLocalWorkSize,  // const size_t *local_work_size
+        0,                 // cl_uint num_events_in_wait_list
+        nullptr,           // const cl_event *event_wait_list
+        nullptr);          // cl_event *event
+    checkError(argc, argv, ciErr1, "Error in clEnqueueNDRangeKernel");
 
-		// Synchronous/blocking read of results, and check accumulated errors
-		ciErr1 = clEnqueueReadBuffer(cqCommandQueue,// cl_command_queue command_queue
-									 cmDevDst,		// cl_mem buffer
-									 CL_TRUE, 		// cl_bool blocking_write
-									 0,		  		// size_t offset
-									 sizeof(cl_char)* 1024, // size_t cb
-									 dst,		// const void *ptr
-									 0, 	  	// cl_uint num_events_in_wait_list
-									 nullptr, 	// const cl_event *event_wait_list
-									 nullptr);	// cl_event *event
-		checkError(argc, argv, ciErr1, "Error in clEnqueueReadBuffer");
+    // Synchronous/blocking read of results, and check accumulated errors
+    ciErr1 =
+        clEnqueueReadBuffer(cqCommandQueue, // cl_command_queue command_queue
+                            cmDevDst,       // cl_mem buffer
+                            CL_TRUE,        // cl_bool blocking_write
+                            0,              // size_t offset
+                            sizeof(cl_char) * 1024, // size_t cb
+                            dst,                    // const void *ptr
+                            0,        // cl_uint num_events_in_wait_list
+                            nullptr,  // const cl_event *event_wait_list
+                            nullptr); // cl_event *event
+    checkError(argc, argv, ciErr1, "Error in clEnqueueReadBuffer");
 
+    // done
+    auto dst_c = (char *)dst;
+    if (*dst_c == 'f') {
+      std::cout << dst_c << '\n';
+      break;
+    }
+  }
 
-		// done
-		auto dst_c = (char*)dst;
-		if (*dst_c == 'f'){
-			std::cout << dst_c << '\n';
-			break;
-		}
-	}
+  system("pause");
 
-	system("pause");
+  // Cleanup and leave
+  Cleanup(argc, argv, EXIT_SUCCESS);
 
-	// Cleanup and leave
-	Cleanup(argc, argv, EXIT_SUCCESS);
-
-	return 0;
+  return 0;
 }
 
-void Cleanup(int argc, char **argv, int iExitCode){
-	// Cleanup allocated objects
-	std::cout << "Starting Cleanup" << '\n';
-	if (cSourceCL)free(cSourceCL);
-	if (ckKernel)clReleaseKernel(ckKernel);
-	if (cpProgram)clReleaseProgram(cpProgram);
-	if (cqCommandQueue)clReleaseCommandQueue(cqCommandQueue);
-	if (cxGPUContext)clReleaseContext(cxGPUContext);
-	if (cmDevDst)clReleaseMemObject(cmDevDst);
+void Cleanup(int argc, char **argv, int iExitCode) {
+  // Cleanup allocated objects
+  std::cout << "Starting Cleanup" << '\n';
+  if (cSourceCL)
+    free(cSourceCL);
+  if (ckKernel)
+    clReleaseKernel(ckKernel);
+  if (cpProgram)
+    clReleaseProgram(cpProgram);
+  if (cqCommandQueue)
+    clReleaseCommandQueue(cqCommandQueue);
+  if (cxGPUContext)
+    clReleaseContext(cxGPUContext);
+  if (cmDevDst)
+    clReleaseMemObject(cmDevDst);
 
-	// Free host memory
-	free(dst);
+  // Free host memory
+  free(dst);
 
-	__shrQAFinish(argc, argv, (iExitCode == EXIT_SUCCESS) ? QA_PASSED : QA_FAILED);
-	exit(iExitCode);
+  __shrQAFinish(argc, argv,
+                (iExitCode == EXIT_SUCCESS) ? QA_PASSED : QA_FAILED);
+  exit(iExitCode);
 }
 
-void checkError(int argc, char **argv, cl_int ciErr, char const * msg){
-	if (ciErr != CL_SUCCESS)	{
-		std::cout << msg << "; status: " << ciErr << '\n';
-		Cleanup(argc, argv, EXIT_FAILURE);
-	}
+void checkError(int argc, char **argv, cl_int ciErr, char const *msg) {
+  if (ciErr != CL_SUCCESS) {
+    std::cout << msg << "; status: " << ciErr << '\n';
+    Cleanup(argc, argv, EXIT_FAILURE);
+  }
 }
--- src/OpenCL_utils.h
+++ src/OpenCL_utils.h
@@ -2,29 +2,25 @@
 #include <stdio.h>
 #include <strings.h>
 
-enum eQAstatus {
-	QA_FAILED = 0,
-	QA_PASSED = 1,
-	QA_WAIVED = 2
-};
+enum eQAstatus { QA_FAILED = 0, QA_PASSED = 1, QA_WAIVED = 2 };
 
-size_t shrRoundUp(int group_size, int global_size){
-	int r = global_size % group_size;
-	if (r == 0)	{
-		return global_size;
-	} else {
-		return global_size + group_size - r;
-	}
+size_t shrRoundUp(int group_size, int global_size) {
+  int r = global_size % group_size;
+  if (r == 0) {
+    return global_size;
+  } else {
+    return global_size + group_size - r;
+  }
 }
 
 // Helper function to init data arrays
-void shrFillArray(float* pfData, int iSize){
-	int i;
-	// const float fScale = 1.0f / (float)RAND_MAX;
-	for (i = 0; i < iSize; ++i){
-		//pfData[i] = fScale * rand();
-		pfData[i] = (float)i;
-	}
+void shrFillArray(float *pfData, int iSize) {
+  int i;
+  // const float fScale = 1.0f / (float)RAND_MAX;
+  for (i = 0; i < iSize; ++i) {
+    // pfData[i] = fScale * rand();
+    pfData[i] = (float)i;
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -32,67 +28,69 @@ void shrFillArray(float* pfData, int iSize){
 //!
 //! @return the source string if succeeded, 0 otherwise
 //! @param cFilename        program filename
-//! @param cPreamble        code that is prepended to the loaded file, typically a set of #defines or a header
+//! @param cPreamble        code that is prepended to the loaded file, typically
+//a set of #defines or a header
 //! @param szFinalLength    returned length of the code string
 //////////////////////////////////////////////////////////////////////////////
-char* oclLoadProgSource(const char* cFilename, const char* cPreamble, size_t* szFinalLength){
-	// locals
-	FILE* pFileStream = NULL;
-	size_t szSourceLength;
+char *oclLoadProgSource(const char *cFilename, const char *cPreamble,
+                        size_t *szFinalLength) {
+  // locals
+  FILE *pFileStream = NULL;
+  size_t szSourceLength;
 
-	// open the OpenCL source code file
-#ifdef _MSC_VER   // Visual studio
-	if (fopen_s(&pFileStream, cFilename, "rb") != 0)	{
-		return NULL;
-	}
-#else           // Linux version
-	pFileStream = fopen(cFilename, "rb");
-	if (pFileStream == 0){
-		return NULL;
-	}
+// open the OpenCL source code file
+#ifdef _MSC_VER // Visual studio
+  if (fopen_s(&pFileStream, cFilename, "rb") != 0) {
+    return NULL;
+  }
+#else // Linux version
+  pFileStream = fopen(cFilename, "rb");
+  if (pFileStream == 0) {
+    return NULL;
+  }
 #endif
 
-	size_t szPreambleLength = strlen(cPreamble);
+  size_t szPreambleLength = strlen(cPreamble);
 
-	// get the length of the source code
-	fseek(pFileStream, 0, SEEK_END);
-	szSourceLength = ftell(pFileStream);
-	fseek(pFileStream, 0, SEEK_SET);
+  // get the length of the source code
+  fseek(pFileStream, 0, SEEK_END);
+  szSourceLength = ftell(pFileStream);
+  fseek(pFileStream, 0, SEEK_SET);
 
-	// allocate a buffer for the source code string and read it in
-	char* cSourceString = (char *)malloc(szSourceLength + szPreambleLength + 1);
-	memcpy(cSourceString, cPreamble, szPreambleLength);
-	if (fread((cSourceString)+szPreambleLength, szSourceLength, 1, pFileStream) != 1){
-		fclose(pFileStream);
-		free(cSourceString);
-		return 0;
-	}
+  // allocate a buffer for the source code string and read it in
+  char *cSourceString = (char *)malloc(szSourceLength + szPreambleLength + 1);
+  memcpy(cSourceString, cPreamble, szPreambleLength);
+  if (fread((cSourceString) + szPreambleLength, szSourceLength, 1,
+            pFileStream) != 1) {
+    fclose(pFileStream);
+    free(cSourceString);
+    return 0;
+  }
 
-	// close the file and return the total length of the combined (preamble + source) string
-	fclose(pFileStream);
-	if (szFinalLength != 0){
-		*szFinalLength = szSourceLength + szPreambleLength;
-	}
-	cSourceString[szSourceLength + szPreambleLength] = '\0';
+  // close the file and return the total length of the combined (preamble +
+  // source) string
+  fclose(pFileStream);
+  if (szFinalLength != 0) {
+    *szFinalLength = szSourceLength + szPreambleLength;
+  }
+  cSourceString[szSourceLength + szPreambleLength] = '\0';
 
-	return cSourceString;
+  return cSourceString;
 }
 
+inline int findExeNameStart(const char *exec_name) {
+  int exename_start = (int)strlen(exec_name);
 
-inline int findExeNameStart(const char *exec_name){
-	int exename_start = (int)strlen(exec_name);
+  while ((exename_start > 0) && (exec_name[exename_start] != '\\') &&
+         (exec_name[exename_start] != '/')) {
+    exename_start--;
+  }
 
-	while ((exename_start > 0) &&
-		     (exec_name[exename_start] != '\\') &&
-		     (exec_name[exename_start] != '/'))	{
-		exename_start--;
-	}
-
-	if (exec_name[exename_start] == '\\' || exec_name[exename_start] == '/'){
-		return exename_start + 1;
-	} else {
-		return exename_start;
-	}
+  if (exec_name[exename_start] == '\\' || exec_name[exename_start] == '/') {
+    return exename_start + 1;
+  } else {
+    return exename_start;
+  }
 }
 
 #ifndef STRCASECMP
@@ -100,54 +98,61 @@ inline int findExeNameStart(const char *exec_name){
 #define STRCASECMP _stricmp
 #else
 // for some reason it can't find strcasecmp on mingw..
-int __str_ccmp(const char *s1, const char *s2){
-	const unsigned char *p1 = (const unsigned char *)s1;
-	const unsigned char *p2 = (const unsigned char *)s2;
-	while (toupper(*p1) == toupper(*p2)) {
-		if (*p1 == '\0') return 0;
- 		++p1;
- 		++p2;
-	}
-	return toupper(*p2) > toupper(*p1) ? -1 : 1;
+int __str_ccmp(const char *s1, const char *s2) {
+  const unsigned char *p1 = (const unsigned char *)s1;
+  const unsigned char *p2 = (const unsigned char *)s2;
+  while (toupper(*p1) == toupper(*p2)) {
+    if (*p1 == '\0')
+      return 0;
+    ++p1;
+    ++p2;
+  }
+  return toupper(*p2) > toupper(*p1) ? -1 : 1;
 }
 #define STRCASECMP __str_ccmp
 #endif
 #endif
 
-inline void __shrQAFinish(int argc, char **argv, int iStatus){
-	// By default QATest is disabled and NoPrompt is Enabled (times out at seconds passed into __ExitInTime() )
-	bool bQATest = false, bNoPrompt = true, bQuitInTime = true;
-	const char *sStatus[] = { "FAILED", "PASSED", "WAIVED", NULL };
+inline void __shrQAFinish(int argc, char **argv, int iStatus) {
+  // By default QATest is disabled and NoPrompt is Enabled (times out at seconds
+  // passed into __ExitInTime() )
+  bool bQATest = false, bNoPrompt = true, bQuitInTime = true;
+  const char *sStatus[] = {"FAILED", "PASSED", "WAIVED", NULL};
 
-	for (int i = 1; i < argc; i++) {
-		int string_start = 0;
-		while (argv[i][string_start] == '-')
-			string_start++;
+  for (int i = 1; i < argc; i++) {
+    int string_start = 0;
+    while (argv[i][string_start] == '-')
+      string_start++;
 
-		const char *string_argv = &argv[i][string_start];
-		if (!STRCASECMP(string_argv, "qatest")) {
-			bQATest = true;
-		}
-		// For SDK individual samples that don't specify -noprompt or -prompt,
-		// a 3 second delay will happen before exiting, giving a user time to view results
-		if (!STRCASECMP(string_argv, "noprompt") || !STRCASECMP(string_argv, "help")) {
-			bNoPrompt = true;
-			bQuitInTime = false;
-		}
-		if (!STRCASECMP(string_argv, "prompt")) {
-			bNoPrompt = false;
-			bQuitInTime = false;
-		}
-	}
+    const char *string_argv = &argv[i][string_start];
+    if (!STRCASECMP(string_argv, "qatest")) {
+      bQATest = true;
+    }
+    // For SDK individual samples that don't specify -noprompt or -prompt,
+    // a 3 second delay will happen before exiting, giving a user time to view
+    // results
+    if (!STRCASECMP(string_argv, "noprompt") ||
+        !STRCASECMP(string_argv, "help")) {
+      bNoPrompt = true;
+      bQuitInTime = false;
+    }
+    if (!STRCASECMP(string_argv, "prompt")) {
+      bNoPrompt = false;
+      bQuitInTime = false;
+    }
+  }
 
-	int exename_start = findExeNameStart(argv[0]);
-	if (bQATest) {
-		fprintf(stdout, "&&&& %s %s", sStatus[iStatus], &(argv[0][exename_start]));
-		for (int i = 1; i < argc; i++) fprintf(stdout, " %s", argv[i]);
-		fprintf(stdout, "\n");
-	} else {
-		fprintf(stdout, "[%s] test results...\n%s\n", &(argv[0][exename_start]), sStatus[iStatus]);
-	}
-	fflush(stdout);
-	printf("\n"); fflush(stdout);
+  int exename_start = findExeNameStart(argv[0]);
+  if (bQATest) {
+    fprintf(stdout, "&&&& %s %s", sStatus[iStatus], &(argv[0][exename_start]));
+    for (int i = 1; i < argc; i++)
+      fprintf(stdout, " %s", argv[i]);
+    fprintf(stdout, "\n");
+  } else {
+    fprintf(stdout, "[%s] test results...\n%s\n", &(argv[0][exename_start]),
+            sStatus[iStatus]);
+  }
+  fflush(stdout);
+  printf("\n");
+  fflush(stdout);
 }
